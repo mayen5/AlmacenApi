46695f6d0a409738a471297f4211ce86a528968d
Carmelo Mayen <carmelo5mayen@gmail.com>
2019-06-08 15:05:14 -0600

    CategoriaController pageable Added

--- src/main/java/com/cmayen/almacen/core/controllers/CategoriaRestController.java
+++ src/main/java/com/cmayen/almacen/core/controllers/CategoriaRestController.java
@@ -2,7 +2,11 @@ package com.cmayen.almacen.core.controllers;
 
 import com.cmayen.almacen.core.models.entity.Categoria;
 import com.cmayen.almacen.core.models.services.ICategoriaService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,11 @@ public class CategoriaRestController {
     @GetMapping("/categorias")
     public List<Categoria> index() {
         return this.categoriaService.findAll();
+    }
+
+    @GetMapping("/categorias/page/{page}")
+    public Page<Categoria> index(@PathVariable Integer page){
+        Pageable pageable = PageRequest.of(page, 5);
+        return categoriaService.findAll(pageable);
     }
 }
